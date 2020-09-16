38a4a9711c332d79bcb744d10b702fea2b51602a
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-09-16 16:48:05 -0300

    Adding methods to edit and delete user.

--- src/main/java/com/example/cache/controller/UserController.java
+++ src/main/java/com/example/cache/controller/UserController.java
@@ -3,8 +3,11 @@ package com.example.cache.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +35,27 @@ public class UserController {
 	public ResponseEntity<Object> save(@RequestBody User user) {
 		try {
 			userRepository.save(user);
-			return ResponseEntity.status(HttpStatus.OK).body(user);
+			return getAll();
+		} catch(RuntimeException r) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(r.getMessage());
+		}
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<Object> update(@RequestBody User user) {
+		try {
+			userRepository.update(user);
+			return getAll();
+		} catch(RuntimeException r) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(r.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> delete(@PathVariable("id") final String id) {
+		try {
+			userRepository.delete(id);
+			return getAll();
 		} catch(RuntimeException r) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(r.getMessage());
 		}
